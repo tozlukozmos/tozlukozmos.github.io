a08035e324eba62efd7bb9bfd9fa1f33399628b8
Abdulkadir Eyigül <aeyigul@outlook.com>
2022-06-10 19:18:31 +0300

    env file obfuscated

--- js/env.js
+++ js/env.js
@@ -1,5 +1,1 @@
-export const env = {
-    'PUBLIC_KEY': 'user_M3NN4ueIo65HbVc0XzSnp',
-    'SERVICE_ID': 'tozlukozmos_secret_id',
-    'TEMPLATE_ID': 'contact_form',
-};
+function b(c,d){const e=a();b=function(f,g){f=f-0x11f;let h=e[f];return h;};return b(c,d);}const i=b;(function(c,d){const h=b;const e=c();while(!![]){try{const f=-parseInt(h(0x12b))/0x1*(parseInt(h(0x120))/0x2)+parseInt(h(0x12a))/0x3*(parseInt(h(0x122))/0x4)+-parseInt(h(0x128))/0x5*(parseInt(h(0x129))/0x6)+parseInt(h(0x125))/0x7+-parseInt(h(0x126))/0x8+-parseInt(h(0x123))/0x9+parseInt(h(0x127))/0xa*(parseInt(h(0x11f))/0xb);if(f===d){break;}else{e['push'](e['shift']());}}catch(g){e['push'](e['shift']());}}}(a,0xc3964));function a(){const j=['contact_form','4170166BbKvqR','1451336JQdIZR','50GScDVt','5wqHPhZ','5037432oOUkuj','9VgbYCC','60232eHoXUq','2917695FNRmHn','40FSLqmz','tozlukozmos_secret_id','1885704hLgVOh','2785392QgjqVb'];a=function(){return j;};return a();}export const env={'PUBLIC_KEY':'user_M3NN4ueIo65HbVc0XzSnp','SERVICE_ID':i(0x121),'TEMPLATE_ID':i(0x124)};
